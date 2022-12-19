b8e08ce35866c00ea41b3fb2260b07bc8e1afc07
Erik Russell <erikrussells@gmail.com>
2022-12-19 16:11:04 -0500

    commit tree 78400dc02265a5fcbaad3ec96ff1695eb4eefa4f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    # validate :clickbait?
+
+    # clickbait_words = [
+    #     Won't Believe,
+    #         Secret,
+    #         Top ,
+    #         Guess,
+    #             ]
+
+    # def clickbait?
+    #     if clickbait_words.none? { |word| word.match title}
+    #         errors.add(:title, "must be bait!")
+    #     end
+    # end
+
 end
